ff284acb97fc15f24e035f335f01c0af8cbdfec2
lddavila <lddavila@miners.utep.edu>
2025-08-08 20:30:39 -0600

    added full fp to env

--- run_example_1_creation.sh
+++ run_example_1_creation.sh
@@ -3,5 +3,5 @@
 #SBATCH -p medium 
 #SBATCH -o output.txt 
 #SBATCH -e output.txt
-source ~/my_env/bin/activate
+source /gpfs/scratch/lddavila/spike_gen_work/my_env/bin/activate
 python create_recordings.py
