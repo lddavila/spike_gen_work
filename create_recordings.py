78ce9afdf4919f97db845391428b33360aae0a7c
lddavila <lddavila@miners.utep.edu>
2025-08-08 20:25:49 -0600

    added submit script

--- create_recordings.py
+++ create_recordings.py
@@ -14,14 +14,15 @@ import matplotlib.pyplot as plt
 import spikeinterface.full as si
 from generate_recordings_with_n_neurons import generate_recordings_with_n_neurons 
 
-
+print("Finished importing packages")
 templates_filename = 'templates_300_neuropixels.h5'
 tempgen = mr.load_templates(templates_filename)
-
+print("Finished loading templates")
 number = "1_"
 num_of_exc = 250;
 num_of_inh = 250;
 length_of_recording = 3000;
 noise_level = 3;
 recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-generate_recordings_with_n_neurons(25,25,length_of_recording,noise_level,templates_filename,recording_file_name)
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
