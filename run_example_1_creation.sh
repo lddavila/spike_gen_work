c8a18dd9b554d740d4539cdee6a27f3be1f1f716
lddavila <lddavila@miners.utep.edu>
2025-08-08 20:33:15 -0600

    FINISHED EDITING

--- run_example_1_creation.sh
+++ run_example_1_creation.sh
@@ -3,5 +3,9 @@
 #SBATCH -p medium 
 #SBATCH -o output.txt 
 #SBATCH -e output.txt
+export TMPDIR=/scratch/$USER/temp
+mkdir -p $TMPDIR
 source /gpfs/scratch/lddavila/spike_gen_work/my_env/bin/activate
+echo "Python path: $(which python)"
+python -m pip list | grep MEArec
 python create_recordings.py
