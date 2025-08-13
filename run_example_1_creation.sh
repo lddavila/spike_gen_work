ab445d5cd81cbf402517b4267b5ad7d19a7d0740
lddavila <lddavila@miners.utep.edu>
2025-08-12 20:22:21 -0600

    changed to work on afriedman account

--- run_example_1_creation.sh
+++ run_example_1_creation.sh
@@ -5,7 +5,7 @@
 #SBATCH -e output.txt
 export TMPDIR=/scratch/$USER/temp
 mkdir -p $TMPDIR
-source /gpfs/scratch/lddavila/spike_gen_work/my_env/bin/activate
+source /gpfs/scratch/afriedman/spike_gen_work/my_env/bin/activate
 echo "Python path: $(which python)"
 python -m pip list | grep MEArec
 python create_recordings.py
