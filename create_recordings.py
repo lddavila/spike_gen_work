96be794f6080cf7f861837af1909af799c273542
lddavila <lddavila@miners.utep.edu>
2025-08-12 20:16:10 -0600

    made recordings much smaller

--- create_recordings.py
+++ create_recordings.py
@@ -18,11 +18,54 @@ print("Finished importing packages")
 templates_filename = 'templates_300_neuropixels.h5'
 tempgen = mr.load_templates(templates_filename)
 print("Finished loading templates")
+number = "3_"
+num_of_exc = 250;
+num_of_inh = 250;
+length_of_recording = 300;
+noise_level = 4;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+
+print("finished first Recording")
+
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
+number = "3_"
+num_of_exc = 150;
+num_of_inh = 250;
+length_of_recording = 300;
+noise_level = 4;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+print("Finished Second Recording")
+
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
+number = "1_"
+num_of_exc = 50;
+num_of_inh = 50;
+length_of_recording = 300;
+noise_level = 4;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+print("Finished third Recording")
+
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
 number = "1_"
 num_of_exc = 250;
 num_of_inh = 250;
-length_of_recording = 3000;
-noise_level = 3;
+length_of_recording = 300;
+noise_level = 5;
 recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
 print("Finished setting parameters")
 generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
