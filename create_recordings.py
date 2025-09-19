f07fcfe1e3576cac95962a991363ee14928755aa
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-19 12:59:10 -0600

    created much more recordings

--- create_recordings.py
+++ create_recordings.py
@@ -18,11 +18,11 @@ print("Finished importing packages")
 templates_filename = 'templates_300_neuropixels.h5'
 tempgen = mr.load_templates(templates_filename)
 print("Finished loading templates")
-number = "3_"
-num_of_exc = 250;
-num_of_inh = 250;
+number = "1_"
+num_of_exc = 300;
+num_of_inh = 300;
 length_of_recording = 300;
-noise_level = 4;
+noise_level = 1;
 recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
 print("Finished setting parameters")
 generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
@@ -33,11 +33,11 @@ print("finished first Recording")
 templates_filename = 'templates_300_neuropixels.h5'
 tempgen = mr.load_templates(templates_filename)
 print("Finished loading templates")
-number = "3_"
-num_of_exc = 150;
-num_of_inh = 250;
+number = "2_"
+num_of_exc = 300;
+num_of_inh = 300;
 length_of_recording = 300;
-noise_level = 4;
+noise_level = 2;
 recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
 print("Finished setting parameters")
 generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
@@ -47,11 +47,11 @@ print("Finished Second Recording")
 templates_filename = 'templates_300_neuropixels.h5'
 tempgen = mr.load_templates(templates_filename)
 print("Finished loading templates")
-number = "1_"
-num_of_exc = 50;
-num_of_inh = 50;
+number = "3_"
+num_of_exc = 300;
+num_of_inh = 300;
 length_of_recording = 300;
-noise_level = 4;
+noise_level = 3;
 recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
 print("Finished setting parameters")
 generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
@@ -61,11 +61,85 @@ print("Finished third Recording")
 templates_filename = 'templates_300_neuropixels.h5'
 tempgen = mr.load_templates(templates_filename)
 print("Finished loading templates")
-number = "1_"
-num_of_exc = 250;
-num_of_inh = 250;
+number = "4_"
+num_of_exc = 300;
+num_of_inh = 300;
+length_of_recording = 300;
+noise_level = 4;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
+number = "5_"
+num_of_exc = 300;
+num_of_inh = 300;
 length_of_recording = 300;
 noise_level = 5;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
+number = "6_"
+num_of_exc = 300;
+num_of_inh = 300;
+length_of_recording = 300;
+noise_level = 6;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
+number = "7_"
+num_of_exc = 300;
+num_of_inh = 300;
+length_of_recording = 300;
+noise_level = 7;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
+number = "8_"
+num_of_exc = 300;
+num_of_inh = 300;
+length_of_recording = 300;
+noise_level = 8;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
+number = "9_"
+num_of_exc = 300;
+num_of_inh = 300;
+length_of_recording = 300;
+noise_level = 9;
+recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
+print("Finished setting parameters")
+generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
+
+templates_filename = 'templates_300_neuropixels.h5'
+tempgen = mr.load_templates(templates_filename)
+print("Finished loading templates")
+number = "10_"
+num_of_exc = 300;
+num_of_inh = 300;
+length_of_recording = 300;
+noise_level = 10;
 recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
 print("Finished setting parameters")
 generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
