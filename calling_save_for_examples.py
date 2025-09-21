b12b4d7affa91a2e2840ad4e209588796da99998
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-09-21 14:39:13 -0600

    added dynamic recording creator

--- calling_save_for_examples.py
+++ calling_save_for_examples.py
@@ -13,22 +13,20 @@ from scipy.io import savemat
 import os
 from extract_recordings import extract_recordings
 
+# get a list of all h5 files in the current directory
+list_of_files = [f for f in os.listdir(os.getcwd()) if f.endswith('.h5')]
 
-recording_one = os.path.join(os.getcwd(),"1_100Neuron300SecondRecordingWithLevel4Noise.h5")
-print("Finished setting recording one path")
-extract_recordings(recording_one,os.path.join(os.getcwd(),"100Neuron300SecondRecordingWithLevel4Noise"))
+#remove the template file from the list
+list_of_files.remove("templates_300_neuropixels.h5")
 
+#print the list of files to ensure we got it right
+print("List of files to process: ", list_of_files)
 
-recording_two = os.path.join(os.getcwd(),"1_500Neuron300SecondRecordingWithLevel5Noise.h5")
-print("Finished setting recording two path")
-extract_recordings(recording_one,os.path.join(os.getcwd(),"500Neuron300SecondRecordingWithLevel5Noise"))
-
-recording_three = os.path.join(os.getcwd(),"3_400Neuron300SecondRecordingWithLevel4Noise.h5")
-print("Finished setting recording three path")
-extract_recordings(recording_three,os.path.join(os.getcwd(),"400Neuron300SecondRecordingWithLevel4Noise"))
-
-recording_four = os.path.join(os.getcwd(),"3_500Neuron300SecondRecordingWithLevel4Noise.h5")
-print("Finished setting recording four path")
-extract_recordings(recording_four,os.path.join(os.getcwd(),"500Neuron300SecondRecordingWithLevel4Noise"))
-
+#now process each file in a for loop
+for i in list_of_files:
+    print("Processing file: ", i)
+    recording = os.path.join(os.getcwd(),i)
+    print("Finished setting recording path")
+    extract_recordings(recording,os.path.join(os.getcwd(),i.split(".h5")[0]))
+    print("Finished extracting recording: ", i);
 
