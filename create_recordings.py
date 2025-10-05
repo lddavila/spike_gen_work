5e9e95be284ec7fbbd7f26db2d297778337ef063
Luis David Davila <57918065+lddavila@users.noreply.github.com>
2025-10-04 19:35:05 -0600

    modified to create 30 second test recording

--- create_recordings.py
+++ create_recordings.py
@@ -15,131 +15,16 @@ import spikeinterface.full as si
 from generate_recordings_with_n_neurons import generate_recordings_with_n_neurons 
 
 print("Finished importing packages")
+
 templates_filename = 'templates_300_neuropixels.h5'
 tempgen = mr.load_templates(templates_filename)
 print("Finished loading templates")
-number = "1_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
+number = "0_test"
+num_of_exc = 5;
+num_of_inh = 5;
+length_of_recording = 15;
 noise_level = 1;
 recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
 print("Finished setting parameters")
 generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
 
-print("finished first Recording")
-
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "2_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 2;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
-print("Finished Second Recording")
-
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "3_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 3;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
-print("Finished third Recording")
-
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "4_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 4;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "5_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 5;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
-
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "6_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 6;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "7_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 7;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "8_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 8;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
-
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "9_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 9;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
-
-templates_filename = 'templates_300_neuropixels.h5'
-tempgen = mr.load_templates(templates_filename)
-print("Finished loading templates")
-number = "10_"
-num_of_exc = 300;
-num_of_inh = 300;
-length_of_recording = 300;
-noise_level = 10;
-recording_file_name = number+str(num_of_exc+num_of_inh)+"Neuron"+str(length_of_recording)+"SecondRecordingWithLevel"+str(noise_level)+"Noise.h5"
-print("Finished setting parameters")
-generate_recordings_with_n_neurons(num_of_exc,num_of_inh,length_of_recording,noise_level,templates_filename,recording_file_name)
